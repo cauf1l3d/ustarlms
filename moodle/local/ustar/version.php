637ff3493cd2142d2a0bc255d616554f9021c357
cauf1l3d <adu0107@gmail.com>
2026-09-21 19:00:00 +0300

    release(stage4): complete standards and routes

--- moodle/local/ustar/version.php
+++ moodle/local/ustar/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ustar';
-$plugin->version   = 2026082738;
+$plugin->version   = 2026082741;
 $plugin->requires  = 2024100100;      // Moodle 4.5+; production validated on Moodle 5.1.1+.
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.7.0-rc.34-stage4-route-scope-commands';
+$plugin->release   = '1.7.0-rc.37-stage4-complete';

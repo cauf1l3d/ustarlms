f9d265f8fef91148763821121cf6333ac82b0094
cauf1l3d <adu0107@gmail.com>
2026-09-21 19:31:02 +0300

    Stage 5: mark completion and rewards package complete

--- moodle/local/ustar/version.php
+++ moodle/local/ustar/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ustar';
-$plugin->version   = 2026082742;
+$plugin->version   = 2026082743;
 $plugin->requires  = 2024100100;      // Moodle 4.5+; production validated on Moodle 5.1.1+.
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.7.0-rc.38-stage5-completion-cycles';
+$plugin->release   = '1.7.0-rc.39-stage5-complete';
